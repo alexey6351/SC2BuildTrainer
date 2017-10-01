ac88b23b343dd54de416d3767b0eec527133617b
alexey <alexey6351@gmail.com>
2017-10-01 13:22:19 -0700

    all three races

--- T2S/Builds.cs
+++ T2S/Builds.cs
@@ -1,4 +1,9 @@
-﻿namespace SC2BuildTrainer
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SC2BuildTrainer
 {
     class Builds
     {
@@ -11,64 +16,212 @@
             }
         };
 
-        public static BuildOrder TvP = new BuildOrder
-        {
-            description = "https://www.youtube.com/watch?v=KK4Trit-MvE",
+        public static string _Terran = @"
+            #https://www.youtube.com/watch?v=KK4Trit-MvE
+            00:00 SCV
+            00:18 Supply Depot
+            00:29 Refinery
+            00:45 Barracks
+            01:32 Marine
+            01:33 Orbital Command
+            01:44 Command Center
+            01:51 Reactor
+            01:57 Mule
+            02:00 Factory
+            02:10 Supply Depot
+            02:24 Refinery
+            02:43 Starport
+            02:45 Widow Mine
+            02:58 Orbital Command
+            03:17 Widow Mine
+            03:23 Viking
+            03:29 Barracks
+            03:38 Barracks
+            03:44 Supply Depot
+            03:45 Tech Lab
+            03:46 Tech Lab
+            03:49 Engineering Bay
+            04:08 Switch
+            04:14 +1 Attack
+            04:16 Reactor
+            04:17 Reactor
+            04:24 Stimpack
+            04:30 Supply Depot
+            04:34 Combat Shield
+            04:56 Medivac
+            04:57 Medivac
+            05:01 Widow Mine
+            05:02 Widow Mine
+            05:04 Supply Depot
+            05:17 Supply Depot
+            05:45 Supply Depot
+            05:46 Move out
+            05:53 Medivac
+            05:54 Medivac
+            05:55 Command Center
+            06:08 Refinery
+            06:09 Refinery
+            06:10 Barracks
+            06:12 Barracks
+            06:22 +1 Armor
+            06:53 Supply Depot
+            06:56 Supply Depot
+            07:00 Reactor
+            07:01 Reactor
+        ";
 
-            items = new BuildOrderItem[]
+        public static BuildOrder Terran => ParseBuildOrder(_Terran);
+        public static BuildOrder Zerg => ParseBuildOrder(_ZvX);
+        public static BuildOrder Protoss => ParseBuildOrder(_PvX);
+
+        private static string _PvX = @"
+            0:00	  Probe
+            0:18	  Pylon	  
+            0:37	  Gateway	  
+            0:41	  Assimilator	  
+            1:21	  Nexus	  
+            1:31	  Cybernetics Core	  
+            1:40	  Assimilator	  
+            1:52	  Pylon	  
+            2:07	  Adept	  
+            2:17	  Stargate	  
+            2:23	  Warp Gate	  
+            2:49	  Adept	  
+            2:56	  Oracle	  
+            3:04	  Assimilator	  
+            3:07	  Assimilator	  
+            3:20	  Adept	  
+            3:37	  Pylon	  
+            3:56	  Mothership Core (Chrono Boost)	  
+            4:04	  Pylon	  
+            4:07	  Forge	  
+            4:15	  Fleet Beacon	  
+            4:22	  Stargate	  
+            4:30	  Adept	  
+            4:41	  Pylon	  
+            4:48	  Photon Cannon	  
+            4:58	  Carrier	  
+            5:01	  Photon Cannon	  
+            5:02	  Protoss Air Weapons Level 1	  
+            5:14	  Pylon	  
+            5:22	  Carrier	  
+            5:39	  Graviton Catapult	  
+            5:43	  Pylon	  
+            6:12	  Carrier	  
+            6:13	  Twilight Council	  
+            6:22	  Nexus	  
+            6:28	  Pylon	  
+            6:36	  Carrier	  
+            6:49	  Adept	  
+            6:53	  Resonating Glaives	  
+            7:00	  Gateway	  
+            7:06	  Gateway	  
+            7:14	  Gateway	  
+            7:23	  Pylon	  
+            7:24	  Pylon	  
+            7:26	  Adept	  
+            7:33	  Pylon	  
+            7:38	  Carrier	  
+            8:03	  Assimilator	  
+            8:05	  Pylon	  
+            8:06	  Assimilator	  
+            8:07	  Carrier	  
+            8:25	  Templar Archives	  
+            8:28	  Pylon	  
+            8:48	  Gateway	  
+            8:50	  Gateway	  
+            8:51	  Gateway	  
+            8:56	  Adept	  
+            9:01	  Oracle	  
+            9:05	  Adept x2	  
+            9:06	  Adept	  
+            9:08	  Psionic Storm	  
+            9:18	  High Templar	  
+            9:23	  Gateway	  
+            9:27	  Gateway	  
+            9:30	  High Templar x3	  
+            9:54	  High Templar x2	  
+            9:55	  High Templar        
+        ";
+
+        private static string _ZvX = @"
+            #http://lotv.spawningtool.com/build/56494/
+            0:00	  Drone	  
+            0:14	  Overlord	  
+            0:53	  Hatchery	  
+            1:09	  Spawning Pool	  
+            1:43	  Overlord	  
+            1:58	  Queen	  
+            2:01	  Zergling x4	  
+            2:04	  Queen	  
+            2:27	  Overlord	  
+            2:34	  Queen	  
+            2:40	  Queen	  
+            2:47	  Spine Crawler	  
+            3:18	  Overlord	  
+            3:21	  Extractor x4	  
+            3:35	  Overlord	  
+            3:58	  Lair	  
+            4:06	  Evolution Chamber x2	  
+            4:09	  Roach Warren	  
+            4:33	  Zerg Missile Weapons Level 1	  
+            4:35	  Zerg Ground Armor Level 1	  
+            4:45	  Hatchery	  
+            4:51	  Roach x7	  
+            4:57	  Roach	  
+            5:00	  Glial Reconstitution	  
+            5:03	  Overseer	  
+            5:09	  Roach x2	  
+            6:00	  Spore Crawler x2	  
+            6:02	  Overseer	  
+            6:23	  Ravager x4	  
+            6:29	  Spore Crawler	  
+            7:03	  Extractor x2
+        ";
+
+
+
+        private static BuildOrder ParseBuildOrder(string buildOrderText)
+        {
+            var buildOrder = new BuildOrder();
+
+            using (var buildOrderStringStream = new StringReader(buildOrderText))
             {
-                    new BuildOrderItem { timing = "00:00:00", text = "SCV" },
-                    new BuildOrderItem { timing = "00:00:18", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:00:29", text = "Refinery" },
-                    new BuildOrderItem { timing = "00:00:45", text = "Barracks" },
-                    new BuildOrderItem { timing = "00:01:32", text = "Marine" },
-                    new BuildOrderItem { timing = "00:01:33", text = "Orbital Command" },
-                    new BuildOrderItem { timing = "00:01:44", text = "Command Center" },
-                    new BuildOrderItem { timing = "00:01:51", text = "Reactor" },
-                    new BuildOrderItem { timing = "00:01:57", text = "Mule" },
-                    new BuildOrderItem { timing = "00:02:00", text = "Factory" },
-                    new BuildOrderItem { timing = "00:02:10", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:02:24", text = "Refinery" },
-                    new BuildOrderItem { timing = "00:02:43", text = "Starport" },
-                    new BuildOrderItem { timing = "00:02:45", text = "Widow Mine" },
-                    new BuildOrderItem { timing = "00:02:58", text = "Orbital Command" },
-                    new BuildOrderItem { timing = "00:03:17", text = "Widow Mine" },
-                    new BuildOrderItem { timing = "00:03:23", text = "Viking" },
-                    new BuildOrderItem { timing = "00:03:29", text = "Barracks" },
-                    new BuildOrderItem { timing = "00:03:38", text = "Barracks" },
-                    new BuildOrderItem { timing = "00:03:44", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:03:45", text = "Tech Lab" },
-                    new BuildOrderItem { timing = "00:03:46", text = "Tech Lab" },
-                    new BuildOrderItem { timing = "00:03:49", text = "Engineering Bay" },
-                    new BuildOrderItem { timing = "00:04:08", text = "Switch" },
-                    new BuildOrderItem { timing = "00:04:14", text = "+1 Attack" },
-                    new BuildOrderItem { timing = "00:04:16", text = "Reactor" },
-                    new BuildOrderItem { timing = "00:04:17", text = "Reactor" },
-                    new BuildOrderItem { timing = "00:04:24", text = "Stimpack" },
-                    new BuildOrderItem { timing = "00:04:30", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:04:34", text = "Combat Shield" },
-                    new BuildOrderItem { timing = "00:04:56", text = "Medivac" },
-                    new BuildOrderItem { timing = "00:04:57", text = "Medivac" },
-                    new BuildOrderItem { timing = "00:05:01", text = "Widow Mine" },
-                    new BuildOrderItem { timing = "00:05:02", text = "Widow Mine" },
-                    new BuildOrderItem { timing = "00:05:04", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:05:17", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:05:45", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:05:46", text = "Move out" },
-                    new BuildOrderItem { timing = "00:05:53", text = "Medivac" },
-                    new BuildOrderItem { timing = "00:05:54", text = "Medivac" },
-                    new BuildOrderItem { timing = "00:05:55", text = "Command Center" },
-                    new BuildOrderItem { timing = "00:06:08", text = "Refinery" },
-                    new BuildOrderItem { timing = "00:06:09", text = "Refinery" },
-                    new BuildOrderItem { timing = "00:06:10", text = "Barracks" },
-                    new BuildOrderItem { timing = "00:06:12", text = "Barracks" },
-                    //new BuildOrderItem { timing = "00:0:", text = "Concussive Shells" },
-                    new BuildOrderItem { timing = "00:06:22", text = "+1 Armor" },
-                    new BuildOrderItem { timing = "00:06:53", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:06:56", text = "Supply Depot" },
-                    new BuildOrderItem { timing = "00:07:00", text = "Reactor" },
-                    new BuildOrderItem { timing = "00:07:01", text = "Reactor" },
+                var buildOrderItems = new List<BuildOrderItem> { };
+
+                string line;
+                while ((line = buildOrderStringStream.ReadLine()) != null)
+                {
+                    if (String.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    line = line.Trim();
+
+                    if (line.StartsWith("#"))
+                    {
+                        buildOrder.description = line.Remove(0, 1);
+                        continue;
+                    }
+
+                    var match = Regex.Match(line, @"^(\d+):(\d+)\s+(.*)$");
+                    if (!match.Success)
+                        throw new ApplicationException();
+
+                    var item = new BuildOrderItem
+                    {
+                        timing = String.Format("00:{0:D2}:{1:D2}", 
+                            Convert.ToInt32(match.Groups[1].Value),
+                            Convert.ToInt32(match.Groups[2].Value)),
+                        text = match.Groups[3].Value
+                    };
+
+                    buildOrderItems.Add(item);
+                }
+
+                buildOrder.items = buildOrderItems.ToArray();
             }
-        };
+
+            return buildOrder;
+        }
     }
 }
--- T2S/Program.cs
+++ T2S/Program.cs
@@ -12,6 +12,9 @@ using System.Windows.Forms;
 using Timer = System.Threading.Timer;
 using System.Drawing;
 using Tesseract;
+using System.Text.RegularExpressions;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace SC2BuildTrainer
 {
@@ -31,48 +34,97 @@ namespace SC2BuildTrainer
     {
         static void Main(string[] args)
         {
-            ExecuteBuildOrder(Builds.Empty);
+            ExecuteBuildOrder(Rectangle.FromLTRB(1750, 15, 1860, 50));
         }
 
-        static void ExecuteBuildOrder(BuildOrder buildOrder)
-        { 
-            var ocrEngine = new TesseractEngine(@"C:\Program Files (x86)\Tesseract-OCR\tessdata", "eng");
-            ocrEngine.SetVariable("tessedit_char_whitelist", "0123456789");
+        static bool CopyFromScreen(Rectangle screenRegion, Image image)
+        {
+            try
+            {
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    g.CopyFromScreen(
+                        screenRegion.Left, screenRegion.Top,
+                        0, 0,
+                        image.Size,
+                        CopyPixelOperation.SourceCopy);
+                }
 
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static void ExecuteBuildOrder(Rectangle screenRegion)
+        { 
             using (var synthesizer = new SpeechSynthesizer())
             {
                 synthesizer.SelectVoice("Microsoft Zira Desktop");
                 synthesizer.Volume = 100;  // 0...100
-                synthesizer.Rate = 0;
+                synthesizer.Rate = 0; // -10 .. 10
 
                 Timer timer = null;
-                uint currentBuildItem = 0;
+                object timerAccessLock = new object();
                 Stopwatch debugWatch = null;
+
+                BuildOrder buildOrder = Builds.Empty;
+                uint currentBuildItem = 0;
+                int supply1 = 0, supply2 = 0;
 
                 var SupplyTimerCallbackProc = new TimerCallback(delegate (object state)
                 {
-                    // 1920 x 1200
-                    using (Bitmap sc2SupplyScreenCapture = new Bitmap(1830 - 1750, 45 - 20))
+                    using (Bitmap screenCapture = new Bitmap(screenRegion.Width, screenRegion.Height))
                     {
-                        using (Graphics g = Graphics.FromImage(sc2SupplyScreenCapture))
-                            g.CopyFromScreen(
-                                1750, 20,
-                                0, 0,
-                                sc2SupplyScreenCapture.Size,
-                                CopyPixelOperation.SourceCopy);
-
-                        using (var ocrResult = ocrEngine.Process(sc2SupplyScreenCapture, Rect.Empty, PageSegMode.SingleLine))
+                        if (CopyFromScreen(screenRegion, screenCapture))
                         {
-                            string text = ocrResult.GetText().Replace("\n", "");
+                            using (var scaledCapture = ScaleImage(screenCapture, 2))
+                            {
+                                using (var ocrEngine = new TesseractEngine(@"C:\Program Files (x86)\Tesseract-OCR\tessdata", "eng"))
+                                {
+                                    ocrEngine.SetVariable("tessedit_char_whitelist", "0123456789/");
+                                    using (var ocrResult = ocrEngine.Process(scaledCapture, Rect.Empty, PageSegMode.SingleLine))
+                                    {
+                                        string text = ocrResult.GetText().Replace(" ", "").Replace("\n", "");
+                                        var match = Regex.Match(text, @"^(\d\d\d?)/(\d\d\d?)$");
+                                        if (match.Success)
+                                        {
+                                            int newSupply1 = Convert.ToInt32(match.Groups[1].Value);
+                                            int newSupply2 = Convert.ToInt32(match.Groups[2].Value);
 
-                            synthesizer.SpeakAsync(text);
+                                            if (newSupply1 != supply1 || newSupply2 != supply2)
+                                            {
+                                                Console.WriteLine("[{0:D2}:{1:D2}] {2}",
+                                                    debugWatch.Elapsed.Minutes,
+                                                    debugWatch.Elapsed.Seconds,
+                                                    text);
 
-                            Console.WriteLine("[{0:D2}:{1:D2}] {2}",
-                                debugWatch.Elapsed.Minutes,
-                                debugWatch.Elapsed.Seconds,
-                                text);
+                                                if (newSupply2 != supply2 // changed
+                                                    && newSupply2 > newSupply1  // mostly errors
+                                                    && newSupply2 - newSupply1 < 8 
+                                                    && newSupply2 < 200)
+                                                {
+                                                    synthesizer.Speak("Supply!");
+                                                }
+
+                                                supply1 = newSupply1;
+                                                supply2 = newSupply2;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
+
+                    lock (timerAccessLock)
+                    {
+                        if (timer != null)
+                            timer.Change(1000, -1);
+                    }
+
                 });
 
                 var BuildOrderTimerCallbackProc = new TimerCallback(delegate (object state)
@@ -102,7 +154,7 @@ namespace SC2BuildTrainer
                     else
                     {
                         timer.Dispose();
-                        timer = new Timer(SupplyTimerCallbackProc, null, 0, 1000);
+                        timer = new Timer(SupplyTimerCallbackProc, null, 0, -1);
                     }
                 });
 
@@ -112,9 +164,20 @@ namespace SC2BuildTrainer
 
                     var KeyPressCallbackProc = new EventHandler<GlobalKeyboardHookEventArgs>(delegate (object sender, GlobalKeyboardHookEventArgs e)
                     {
-                        if (!running && e.KeyboardData.VirtualCode == 'S')
+                        var vk = e.KeyboardData.VirtualCode;
+
+                        if (!running && 
+                             ( e.KeyboardData.VirtualCode == 'T'
+                             || e.KeyboardData.VirtualCode == 'Z'
+                             || e.KeyboardData.VirtualCode == 'P'))
                         {
                             Console.WriteLine("(Re)starting...");
+                            buildOrder = 
+                                e.KeyboardData.VirtualCode == 'T' ? Builds.Terran
+                                    : e.KeyboardData.VirtualCode == 'Z' ? Builds.Zerg
+                                    : e.KeyboardData.VirtualCode == 'P' ? Builds.Protoss
+                                    : Builds.Empty;
+
                             currentBuildItem = 0;
                             timer = new Timer(BuildOrderTimerCallbackProc, null, 0, -1);
                             debugWatch = Stopwatch.StartNew();
@@ -122,7 +185,11 @@ namespace SC2BuildTrainer
                         }
                         else if (running && e.KeyboardData.VirtualCode == 0x79) // VK_F10
                         {
-                            timer.Dispose();
+                            lock (timerAccessLock)
+                            {
+                                timer.Dispose();
+                                timer = null;
+                            }
                             debugWatch.Stop();
                             running = false;
                             Console.WriteLine("Stopped.");
@@ -137,6 +204,46 @@ namespace SC2BuildTrainer
                    Application.Run();
                 }
             }
+        }
+
+        static Bitmap ScaleImage(Image image, int factor)
+        {
+            if (factor == 1)
+                return new Bitmap(image);
+            else
+                return ResizeImage(image, image.Width * factor, image.Height * factor);
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
     }
 }
